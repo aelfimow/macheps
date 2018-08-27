2364a503cb49e143626fc6e1d79f4d576f87a67c
Alexander Elfimow <my.free.mail@gmx.de>
2018-08-27 22:14:24 +0200

    Added loop counter

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,20 @@ try
     macheps_fp32_init(&param);
 
     bool done = false;
+    size_t loopcnt = 0;
 
     while (!done)
     {
         macheps_fp32_compute(&param);
 
         done = (0 == std::memcmp(&param.value, &param.sum, sizeof(fp32_t)));
+
+        ++loopcnt;
     }
 
-    std::cout << "Machine epsilon (fp32): " << param.epsilon << std::endl;
+    std::cout << "Machine epsilon (fp32): " << std::endl;
+    std::cout << "    Loop counter: " << loopcnt << std::endl;
+    std::cout << "    epsilon: << " << param.epsilon << std::endl;
 
     return EXIT_SUCCESS;
 }
